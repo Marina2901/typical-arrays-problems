76f6c74c77801f0ef762e46b8fa93e062663f165
Marina2901 <marm13@mail.ru>
2021-03-04 22:34:32 +0200

    init: add typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 exports.min = function min (array) {
-  if (array === undefined || array.length == 0){
-    return 0;
-  }else return Math.min(...array);
+  if (!array || (array.length === 0)){
+    return 0
+  } return Math.min(...array);
   
-};
+}
 
 exports.max = function max (array) {
-  if (array === undefined || array.length == 0){
-    return 0;
-  } else return Math.max(... array);
-  };
+  if (!array || (array.length === 0)){
+    return 0
+  } return Math.max(... array);
+  }
 
 exports.avg = function avg (array) {
-if (array === undefined || array.length == 0){
-  return 0;}
+if (!array || (array.length === 0)){
+  return 0}
   else return array.reduce((a,b)=>(a+b)) / array.length;
 }
